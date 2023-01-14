a1b061532e03e6f7a39d70e581388cc3b22bf2d2
Rachel Spencer <rachelspencer688@gmail.com>
2023-01-14 09:30:46 -0500

    context dummy example

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ const App = () => {
 
     const deleteBookById = async (id) => {
         await axios.delete(`http://localhost:3001/books/${id}`);
-        
+
         const updatedBooks = books.filter((book) => {
             return book.id !== id;
         });
